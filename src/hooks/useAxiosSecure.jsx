28e4d8de6d7a7990ee056e92ae6ef3e10db8a10f
Didarul Shahriar <didarul13579@gmail.com>
2025-12-15 12:48:49 +0600

    add axios

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import React, { useEffect } from 'react';
+import useAuth from "./useAuth";
+
+const axiosSecure = axios.create({
+    baseURL: "http://localhost:3002"
+})
+
+const useAxiosSecure = () => {
+    const { user } = useAuth();
+    useEffect(() => {
+        axiosSecure.interceptors.request.use(config => {
+            config.headers.Authorization = `Bearer ${user?.accessToken}`
+            return config;
+        })
+    }, [user])
+
+    return axiosSecure;
+}
+
+export default useAxiosSecure;
