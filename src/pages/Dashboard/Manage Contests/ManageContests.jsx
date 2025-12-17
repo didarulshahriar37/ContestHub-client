2e7446c552ad878e566bb4ace45c365fc16d9c81
Didarul Shahriar <didarul13579@gmail.com>
2025-12-17 20:43:38 +0600

    add delete func for admin

--- src/pages/Dashboard/Manage Contests/ManageContests.jsx
+++ src/pages/Dashboard/Manage Contests/ManageContests.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import Swal from 'sweetalert2';
 import { useQuery } from '@tanstack/react-query';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const ManageContests = () => {
 
@@ -65,6 +66,30 @@ const ManageContests = () => {
         });
     }
 
+    const handleDelete = contest => {
+        Swal.fire({
+            title: "Are you sure?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Delete !"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/manage-contests/${contest._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount) {
+                            refetch();
+                            Swal.fire({
+                                title: `${contest.contest_name} is deleted !`,
+                                icon: "success",
+                            });
+                        }
+                    })
+            }
+        });
+    }
+
     return (
         <div className='bg-base-300 min-h-screen'>
             <h2 className='md:text-4xl text-2xl font-bold text-center mb-10'>Manage Contests ({contests.length})</h2>
@@ -88,18 +113,19 @@ const ManageContests = () => {
                                 <td className='font-bold'>{contest.contest_name}</td>
                                 <td className='font-semibold'>{contest.creator_name} <br /> {contest.creator_email}</td>
                                 <td>{contest.contest_type}</td>
-                                <td className={contest.approval_status === "approved" ? "text-green-500 font-bold" : contest.approval_status === "rejected" ? "text-red-500 font-bold" : "text-blue-500 font-bold"}>{contest.approval_status}</td>
+                                <td className={contest.approval_status === "approved" ? "text-green-500" : contest.approval_status === "rejected" ? "text-red-500" : "text-blue-500"}>{contest.approval_status}</td>
                                 <td>
                                     {
-                                        contest.approval_status === "approved" ? <div className='flex gap-2'>
-                                            <button disabled onClick={() => handleApprove(contest)} className='btn btn-soft btn-success'>Approve</button>
-                                            <button onClick={() => handleReject(contest)} className='btn btn-soft btn-error'>Reject</button>
-                                        </div> : contest.approval_status === "rejected" ? <div className='flex gap-2'>
-                                            <button onClick={() => handleApprove(contest)} className='btn btn- btn-success'>Approve</button>
-                                            <button disabled onClick={() => handleReject(contest)} className='btn btn-soft btn-error'>Reject</button>
+                                        contest.approval_status === "approved" || contest.approval_status === "rejected" ? <div className='flex gap-2'>
+                                            <button disabled onClick={() => handleApprove(contest)} className='btn'>Approve</button>
+                                            <button disabled onClick={() => handleReject(contest)} className='btn'>Reject</button>
+                                            <button onClick={() => handleDelete(contest)} className='btn text-red-500'><FaTrashAlt />
+                                            </button>
                                         </div> : <div className='flex gap-2'>
-                                            <button onClick={() => handleApprove(contest)} className='btn btn-soft btn-success'>Approve</button>
-                                            <button onClick={() => handleReject(contest)} className='btn btn-soft btn-error'>Reject</button>
+                                            <button onClick={() => handleApprove(contest)} className='btn'>Approve</button>
+                                            <button onClick={() => handleReject(contest)} className='btn'>Reject</button>
+                                            <button onClick={() => handleDelete(contest)} className='btn text-red-500'><FaTrashAlt />
+                                            </button>
                                         </div>
                                     }
                                 </td>
